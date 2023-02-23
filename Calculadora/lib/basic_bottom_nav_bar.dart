78c828f210843fd646ac8339ff5f489ef05f2b36
Alexvi011 <alejandropvite@gmail.com>
2023-02-23 17:25:33 -0600

    Se agragan las barras de navegación

---
+++ Calculadora/lib/basic_bottom_nav_bar.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:calculadora/calculadora.dart';
+
+class BasicBottomNavBar extends StatefulWidget {
+  const BasicBottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  _BasicBottomNavBarState createState() => _BasicBottomNavBarState();
+}
+
+class _BasicBottomNavBarState extends State<BasicBottomNavBar> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _pages = <Widget>[
+
+    Icon(
+      Icons.home,
+      size: 150,
+    ),
+    Calculadora(),
+    Icon(
+      Icons.title,
+      size: 150,
+    ),
+    // Icon(
+    //   Icons.chat,
+    //   size: 150,
+    // ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Primera Asignación'),
+      ),
+      body: Center(
+        child: _pages.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calculate),
+            label: 'Calculadora',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.title),
+            label: 'Texto',
+          ),
+          // BottomNavigationBarItem(
+          //   icon: Icon(Icons.chat),
+          //   label: 'Chats',
+          // ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
